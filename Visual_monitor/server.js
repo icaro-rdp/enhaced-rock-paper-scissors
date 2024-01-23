d6bbb966c7c1db64a2bb753b66bed20943bf581b
Icaro Re Depaolini <igher97@gmail.com>
2024-01-23 18:41:11 +0100

    init connessione server e PD

--- Visual_monitor/server.js
+++ Visual_monitor/server.js
@@ -13,7 +13,15 @@ server.on("message", (msg, rinfo) => {
 server.on("listening", () => {
   const address = server.address();
   console.log(`server listening ${address.address}:${address.port}`);
+  // qui chiamo handleMovements e poi passo i risultati
 });
+
+const handleMovements(message:string){
+  // qui arriva il messaggio delle mani singole
+  // il messaggio che arriva è del tipo : p1-0 / p2-3
+  // ritorno un array con le due mosse fatte da p1 e p2
+  // [0,3]
+}
 
 // Listening on a port (e.g., 41234)
 server.bind(41234);
