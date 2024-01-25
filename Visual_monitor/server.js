0f11d3387d86421bd493e69fbc15ed417e1d7577
Icaro Re Depaolini <igher97@gmail.com>
2024-01-25 12:17:29 +0100

    express test

--- Visual_monitor/server.js
+++ Visual_monitor/server.js
@@ -1,34 +1,39 @@
 import dgram from "dgram";
-
-const UDP_server = dgram.createSocket("udp4");
-// Listening on a port (e.g., 41234)
-UDP_server.bind(41234);
-
 import express from "express";
+
+// Server for PD communication
+// const UDP_server = dgram.createSocket("udp4");
+// UDP_server.bind(41234);
+
+// const moves = [undefined, undefined];
+// // gestione dei messaggi che arrivano da PD
+// UDP_server.on("message", (msg) => {
+//   bufferToMoves(moves, msg);
+//   console.log(moves);
+// });
+
+// UDP_server.on("listening", () => {
+//   const address = UDP_server.address();
+//   console.log(`UDP_server listening ${address.address}:${address.port}`);
+// });
+
+// UDP_server.on("error", (err) => {
+//   console.log(`UDP_server error:\n${err.stack}`);
+//   UDP_server.close();
+// });
+
+// Server for client communication
 const express_server = express();
-
-UDP_server.on("error", (err) => {
-  console.log(`UDP_server error:\n${err.stack}`);
-  UDP_server.close();
-});
-
-const moves = [undefined, undefined];
-UDP_server.on("message", (msg) => {
-  // qui chiamo handleMovements e poi passo i risultati
-  handleMovements(moves, msg);
-  console.log(moves);
-});
-
-UDP_server.on("listening", () => {
-  const address = UDP_server.address();
-  console.log(`UDP_server listening ${address.address}:${address.port}`);
-});
 
 express_server.listen(3000, () => {
   console.log("express_server running on port 3000");
 });
 
-function handleMovements(moves, buffer) {
+express_server.get("/moves", (req, res) => {
+  res.json({ move: lastMove });
+});
+
+function bufferToMoves(moves, buffer) {
   const message = buffer.toString();
   const splitted = message.split("-");
   const player = splitted[0];
