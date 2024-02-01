26c14581744dea2964bb873b7d9df2253fa85fba
Icaro Re Depaolini <igher97@gmail.com>
2024-02-01 19:04:06 +0100

    Refactor code to add mode column in games_cleaned.csv

--- Research/Quantitative/Hypothesis_testing/main.py
+++ Research/Quantitative/Hypothesis_testing/main.py
@@ -9,13 +9,19 @@ with open('games.csv', newline='') as f:
     # Removing the header from the list
     file_data = list(reader)[1:]
     mode = ''
+
     for rows in file_data:
-        if "blind" in rows[0] and "not blind" not in rows[0]:
+        if "test" in rows[0] or "tutorial" in rows[0]:
+            mode = 'test'
+            continue
+        elif "blind" in rows[0] and "not blind" not in rows[0]:
             mode = 'blind'
+            continue
         elif "not blind" in rows[0]:
             mode = 'not blind'
+            continue
         rows.append(mode)
-    
+
     # write a new file with the cleaned data
     with open('games_cleaned.csv', 'w', newline='') as f:
         writer = csv.writer(f)
